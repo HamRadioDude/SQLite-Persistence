1eba69a349428ddb456eceb9938ed675147fad38
Sean Klechak <sklechak@students.mchenry.edu>
2019-04-03 17:30:56 -0500

    Initial commit

---
+++ Temporary.h
@@ -0,0 +1,17 @@
+//
+//  Temporary.h
+//  SQLite Persistence
+//
+//  Created by student on 4/3/19.
+//  Copyright © 2019 Sean Klechak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Temporary : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Temporary.m
@@ -0,0 +1,13 @@
+//
+//  Temporary.m
+//  SQLite Persistence
+//
+//  Created by student on 4/3/19.
+//  Copyright © 2019 Sean Klechak. All rights reserved.
+//
+
+#import "Temporary.h"
+
+@implementation Temporary
+
+@end
